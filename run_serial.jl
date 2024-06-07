4d13aa784988beaf9142192d1d333eb550f5ceff
crosstyan <crosstyan@outlook.com>
2024-06-07 14:47:41 +0800

    OCaml like module

--- run_serial.jl
+++ run_serial.jl
@@ -1,9 +1,14 @@
 import LibSerialPort as Serial
 using Logging: @info, @debug, @warn, @error
 using Base: @kwdef
-# https://github.com/JuliaLang/julia/issues/1255
 
-@kwdef struct Target
+# https://github.com/JuliaLang/julia/issues/1255
+# https://en.wikipedia.org/wiki/Multiple_dispatch
+# https://thautwarm.github.io/MLStyle.jl/latest/syntax/pattern.html
+
+module Target
+export coord_si, speed_si, t, unmarshal, show
+@kwdef struct t
     """
     in millimeters, Int16 (MSB is the sign bit) in little endian
     """
@@ -14,24 +19,23 @@ using Base: @kwdef
     """
     speed::Int16
 
-    # UInt16
     resolution::UInt16
 
-    function Target(coord, speed, resolution)
+    function t(coord, speed, resolution)
         new(coord, speed, resolution)
     end
 end
 
-function coord_si(target::Target)::Tuple{Float32,Float32}
+@inline function coord_si(target::t)::Tuple{Float32,Float32}
     x, y = target.coord
     return (x / 1000, y / 1000)
 end
 
-function speed_si(target::Target)::Float32
+@inline function speed_si(target::t)::Float32
     return target.speed / 100
 end
 
-function msb_bit_int16(num::Integer)
+@inline function msb_bit_int16(num::Integer)
     """
     Some genius decided to use the most significant bit as the sign bit
 
@@ -40,21 +44,21 @@ function msb_bit_int16(num::Integer)
     Returns:
         Int16: A 16-bit number (use most significant bit as sign bit)
     """
-    @assert 0 <= num < 2^16
+    @assert 0 <= num < 2^16 "Number out of range for 16-bit unsigned integer"
     sign = num & 0x8000 >= 1
     n = num & 0x7fff
     x = unsafe_trunc(Int16, n)
     return sign ? x : -x
 end
 
-function Base.show(io::IO, target::Target)
+function Base.show(io::IO, target::t)
     x, y = target.coord
     speed = target.speed
     resolution = target.resolution
     println(io, "Target(coord=($x, $y), speed=$speed, resolution=$resolution)")
 end
 
-function unmarshal_target(data::AbstractArray{UInt8})::Union{Target,Nothing}
+function unmarshal(data::AbstractArray{UInt8})::Union{t,Nothing}
     @assert length(data) == 8 "Invalid data length"
     if all(data .== 0)
         return nothing
@@ -71,15 +75,18 @@ function unmarshal_target(data::AbstractArray{UInt8})::Union{Target,Nothing}
 
     resolution = reinterpret(UInt16, data[7:8])[1]
 
-    return Target((x, y), speed, resolution)
+    return t((x, y), speed, resolution)
+end
 end
 
-
-@kwdef struct Targets
-    targets::Vector{Target} = Target[]
+module Targets
+import ..Target
+export t, unmarshal, show
+@kwdef struct t
+    targets::Vector{Target.t} = Target.t[]
 end
 
-function Base.show(io::IO, targets::Targets)
+function Base.show(io::IO, targets::t)
     print(io, "Targets[")
     i = 1
     for target in targets.targets
@@ -95,23 +102,24 @@ function Base.show(io::IO, targets::Targets)
 end
 
 const TARGETS_MAGIC = UInt8[0xaa, 0xff, 0x03, 0x00]
-function unmarshal_targets(data::AbstractArray{UInt8})::Targets
+function unmarshal(data::AbstractArray{UInt8})::t
     offset = 1
     if data[1:4] != TARGETS_MAGIC
         throw(ArgumentError("Invalid magic"))
     end
     offset += 4
-    targets = Target[]
+    targets = Target.t[]
     # we have three targets
     # if the data is all zeros, it means the target is not set
     for _ in 1:3
-        target = unmarshal_target(data[offset:offset+7])
+        target = Target.unmarshal(data[offset:offset+7])
         if !isnothing(target)
             push!(targets, target)
         end
         offset += 8
     end
-    return Targets(targets)
+    return t(targets)
+end
 end
 
 function test_unmarshal()
@@ -120,12 +128,12 @@ function test_unmarshal()
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
         0x00, 0x00, 0x00, 0x00
     ]
-    targets = unmarshal_targets(data)
+    targets = Targets.unmarshal(data)
     @assert length(targets.targets) == 1
     @assert targets.targets[1].coord == (-782, 1713)
     @assert targets.targets[1].speed == -16
     @assert targets.targets[1].resolution == 320
-    println(targets)
+    @info targets
 end
 
 function bytes2hex(arr::AbstractArray{UInt8})::String
@@ -141,10 +149,9 @@ function run_serial()
         Serial.clear_write_timeout(port)
         while true
             data = readuntil(port, ENDING_MAGIC)
-            print("($(length(data))) ")
-            println("$(bytes2hex(data))")
-            targets = unmarshal_targets(data)
-            println(targets)
+            @debug "$(length(data)) $(bytes2hex(data))"
+            targets = Targets.unmarshal(data)
+            @info "$(targets)"
             Serial.clear_read_timeout(port)
         end
     end
