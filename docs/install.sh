b73e5240530105dd9e50323cff3bd2ab035e40c2
crosstyan <crosstyan@outlook.com>
2024-06-26 11:31:33 +0800

    install service

--- docs/install.sh
+++ docs/install.sh
@@ -2,3 +2,6 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cp $SCRIPT_DIR/radar.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable radar
+systemctl start radar
